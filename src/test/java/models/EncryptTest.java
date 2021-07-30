7ac51518ec3b356e0db349050123ded7c2f9c852
clarelimo <clare.limo@student.moringaschool.com>
2021-07-30 16:42:42 +0300

    encrypt test cases

--- src/test/java/models/EncryptTest.java
+++ src/test/java/models/EncryptTest.java
@@ -1,4 +1,30 @@
 package models;
 
+import org.junit.Test;
+
 public class EncryptTest {
+
+    @Test
+    public  void encrypt_onlyString_exception(){
+        //arrange
+        Encrypt encrypt = new Encrypt();
+
+        //act
+        // var input = encrypt.imput;
+
+        //assert
+//            assertEquals(input instanceof String);
+
+    }
+
+    @Test
+    public  void  encrypt_notNull_Exception(){
+
+    }
+
+    @Test
+    public void  encrypt_encryptText_string(){
+        
+    }
+
 }
