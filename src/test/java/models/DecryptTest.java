dd1bc17e3e2c773a9b4f7a02e3c5718f40db2dba
clarelimo <clare.limo@student.moringaschool.com>
2021-07-30 19:30:08 +0300

    decrypt tests done

--- src/test/java/models/DecryptTest.java
+++ src/test/java/models/DecryptTest.java
@@ -4,19 +4,32 @@ import org.junit.Test;
 
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.TestCase.assertEquals;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class DecryptTest {
 
     @Test
     public  void  decrypt_notNull_Exception(){
-
+        Decrypt decrypt = new Decrypt("ABCD",3);
+        assertNotNull(decrypt);
     }
 
     @Test
     public  void  decrypt_onlyString_Exception(){
+        Decrypt decrypt = new Decrypt("BC",1);
+        decrypt.getDecrypted();
+        assertTrue(decrypt.getDecrypted() instanceof String );
+    }
 
+    @Test
+    public void  decrypt_boundaryLength_exception(){
+        try {
+            Decrypt decrypt = new Decrypt("THE QUICK BROWN FOX",30);
+            decrypt.getDecrypted();
+
+        }catch (Exception ex){
+            assertEquals("The key must be between 1 and 25",ex.getMessage());
+        }
     }
 
     @Test
